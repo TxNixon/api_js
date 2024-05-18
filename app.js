3b431e3a99a92f166c4750c367f57ab28eebdb04
admojocahyo <admojocahyo@gmail.com>
2024-05-18 05:40:36 +0000

    ngrok function

--- app.js
+++ app.js
@@ -9,18 +9,16 @@ const PORT = 3030;
 app.use(express.json());
 app.use('/api', todoRoutes);
 
-// Create HTTP server
 const server = http.createServer(app);
 
 server.listen(PORT, async () => {
     console.log(`Server is running on PORT ${PORT}`);
 
     try {
-        // Start Ngrok tunnel
         const listener = await ngrok.connect({ addr: PORT, authtoken_from_env: true });
-        // Call the url function to get the actual URL
         const url = listener.url();
         console.log(`Ngrok tunnel running at: ${url}`);
+        
     } catch (err) {
         console.error('Error starting Ngrok:', err);
     }
